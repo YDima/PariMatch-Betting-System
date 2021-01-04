410035c9eca9f34dfc4952d326e6ba7232c2cc44
YDima <yurchenkodmytroofficial@gmail.com>
2021-01-04 21:23:57 +0200

    Starting Project

---
+++ Betting System/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Betting System
+//
+//  Created by Dmytro Yurchenko on 4.1.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view.
+     }
+
+
+}
+

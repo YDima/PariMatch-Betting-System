fdfafe17601ee36bc76fccc5c7d36a36e1aca33e
Kvadrot <send.me.your.maild@gmail.com>
2021-01-06 13:25:22 +0200

    adding regularUser funcionality

--- Betting System/Models/RegularUser.swift
+++ Betting System/Models/RegularUser.swift
@@ -10,22 +10,25 @@ import UIKit
 protocol RegularUserProtocol {
      func placeBet(bet: Bet)
      
-     func printAllBets()
+     func viewAllUserBets()
 }
 
 class RegularUser: User, RegularUserProtocol {
-     
+
      private var bets: [Bet] = []
-     
+     private var storage = Storage()
+    var currentUser: User
+
      override init(username: String, password: String, role: Role = .regular, state: State) {
           super.init(username: username, password: password, role: role, state: state)
      }
-     
+
      func placeBet(bet: Bet) {
-          
+        bets.append(bet)
+        storage.addBetsToUser( currentUser: currentUser, bets: bets)
      }
-     
-     func printAllBets() {
-          
+
+     func viewAllUserBets() {
+        storage.viewAllUserBets()
      }
 }
--- Betting System/Models/Storage.swift
+++ Betting System/Models/Storage.swift
@@ -9,7 +9,8 @@ import Foundation
 
 class Storage {
     var bets: [Bet] = []
-    
+    var userBets: [String : [Bet]] = [:]
+
     init() {
         bets.append(Bet(name: "bet0"))
         bets.append(Bet(name: "bet1"))
@@ -19,5 +20,13 @@ class Storage {
         bets.append(Bet(name: "bet5"))
         bets.append(Bet(name: "bet6"))
         bets.append(Bet(name: "bet7"))
+    }
+
+    func addBetsToUser(currentUser: User, bets: [Bet]) {
+        //Реализуй логику)
+    }
+
+    func viewAllUserBets() {
+        //
     }
 }

a84d250dc6f008ff74aa4fb4a47711529bf47e3b
Admin <admin@192.168.235.130>
2021-01-09 17:08:33 +0300

    Extend storage functionality

--- Betting System/Models/Storage.swift
+++ Betting System/Models/Storage.swift
@@ -8,40 +8,62 @@
 import Foundation
 
 class Storage {
-     var bets: [Bet] = []
-     var userBets: [String : [Bet]] = [:]
-
-     init() {
-          bets.append(Bet(name: "bet0"))
-          bets.append(Bet(name: "bet1"))
-          bets.append(Bet(name: "bet2"))
-          bets.append(Bet(name: "bet3"))
-          bets.append(Bet(name: "bet4"))
-          bets.append(Bet(name: "bet5"))
-          bets.append(Bet(name: "bet6"))
-          bets.append(Bet(name: "bet7"))
-     }
-     
+    
+    var bets: [Bet] = []
+    var userBets: [String : [Bet]] = [:]
+    private var users: [String: User] = [:]
+    
+    init() {
+        bets.append(Bet(name: "bet0"))
+        bets.append(Bet(name: "bet1"))
+        bets.append(Bet(name: "bet2"))
+        bets.append(Bet(name: "bet3"))
+        bets.append(Bet(name: "bet4"))
+        bets.append(Bet(name: "bet5"))
+        bets.append(Bet(name: "bet6"))
+        bets.append(Bet(name: "bet7"))
+    }
+    
+    func isBanned(name: String) -> Bool {
+        return users[name]?.state == .banned
+    }
+    
+    func checkUser(name: String, password: String) -> Bool {
+        guard let user = users[name] else {
+            return false
+        }
+        
+        if user.password == password {
+            return true
+        }
+        
+        return false
+    }
+    
 }
 
 //MARK: - RegularUser
 extension Storage {
-     func addBetsToUser(username: String, bets: [Bet]) {
-          userBets.updateValue(bets, forKey: username)
-     }
-
-     func viewAllUserBets() {
-          
-     }
+    
+    func addBetsToUser(username: String, bets: [Bet]) {
+        userBets.updateValue(bets, forKey: username)
+    }
+    
+    func viewAllUserBets() {
+        
+    }
+    
 }
 
 //MARK: - Admin
 extension Storage {
-     func banUser() {
-      
-     }
-
-     func browseUsers() {
-      
-     }
+    
+    func banUser() {
+        
+    }
+    
+    func browseUsers() {
+        
+    }
+    
 }
